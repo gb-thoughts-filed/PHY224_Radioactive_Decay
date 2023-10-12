3a1e932fbad7162d318b02e93b2140e2fabfb316
Junyu11111 <john2double@gmail.com>
2023-10-12 18:35:09 -0400

    poisson distribution method fixed

--- Part_Two_Plate.py
+++ Part_Two_Plate.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy
 import numpy as np
 from scipy import stats
 from scipy import special
@@ -25,21 +26,23 @@ def histogram_plot(data, rounded_range, interval):
 
 
 def poisson_distribution(num_of_counts, average):
-    return np.exp(-average) * (average ** num_of_counts) / special.gamma(num_of_counts + 1)
+    return np.exp(-average) * numpy.power(average, np.longdouble(num_of_counts), dtype=np.float128) / special.gamma(num_of_counts + 1)
+    # dtype=np.float128 added for bigger power calculations.
+    # If AttributeError: module 'numpy' has no attribute 'float128' is returned, change it to dtype=np.float64
 
 
 if __name__ == "__main__":
     sample_num_plate, num_of_counts_plate = np.loadtxt(os.curdir + "/2022_10_05_pm_plate.txt", skiprows=2, unpack=True)
     sample_num_background, num_of_counts_background = np.loadtxt(os.curdir + "/2022_10_05_pm_background.txt",
                                                                  skiprows=2, unpack=True)
-    count_rate = (num_of_counts_plate - np.average(num_of_counts_background))  # delta t given by data description
+    count_rate = (num_of_counts_plate - np.average(num_of_counts_background))  # delta t given by data
+    # description
     sorted_count_rate = np.sort(count_rate)
     average_count_rate = np.average(count_rate)
     range_1 = (floor_dig(min(count_rate), 0), ceil_dig(max(count_rate), 0))
     plt.figure("Histogram")
-    histogram_plot(count_rate, range_1, 5)
+    histogram_plot(count_rate, range_1, 1)
     print(count_rate, sorted_count_rate)
-
     print(average_count_rate)
 
     poisson_probabilities = poisson_distribution(sorted_count_rate, average_count_rate)
