81a2b4c0cf6e0dea3ca8b6c848e0b329d4cb29e6
Junyu11111 <john2double@gmail.com>
2023-10-12 17:55:55 -0400

    poisson distribution method change

--- Part_Two_Plate.py
+++ Part_Two_Plate.py
@@ -2,6 +2,7 @@ import os
 
 import numpy as np
 from scipy import stats
+from scipy import special
 import matplotlib.pyplot as plt
 
 
@@ -23,28 +24,28 @@ def histogram_plot(data, rounded_range, interval):
              , edgecolor='black', linewidth=1.2)
 
 
+def poisson_distribution(num_of_counts, average):
+    return np.exp(-average) * (average ** num_of_counts) / special.gamma(num_of_counts + 1)
+
+
 if __name__ == "__main__":
     sample_num_plate, num_of_counts_plate = np.loadtxt(os.curdir + "/2022_10_05_pm_plate.txt", skiprows=2, unpack=True)
     sample_num_background, num_of_counts_background = np.loadtxt(os.curdir + "/2022_10_05_pm_background.txt",
                                                                  skiprows=2, unpack=True)
-    count_rate = (num_of_counts_plate - np.average(num_of_counts_background)) / 5  # delta t given by data description
+    count_rate = (num_of_counts_plate - np.average(num_of_counts_background))  # delta t given by data description
     sorted_count_rate = np.sort(count_rate)
-    range_1 = (np.floor(min(count_rate)), np.ceil(max(count_rate)))
+    average_count_rate = np.average(count_rate)
+    range_1 = (floor_dig(min(count_rate), 0), ceil_dig(max(count_rate), 0))
     plt.figure("Histogram")
-    histogram_plot(count_rate, range_1, 1)
+    histogram_plot(count_rate, range_1, 5)
     print(count_rate, sorted_count_rate)
-    average_num_of_counts = np.average(count_rate)
-    print(average_num_of_counts)
 
-    poisson_probabilities = stats.poisson.pmf(np.round(sorted_count_rate), np.round(average_num_of_counts))
-    print(poisson_probabilities)
-    plt.plot(sorted_count_rate, poisson_probabilities, "rx", label="Poisson distribution")
+    print(average_count_rate)
 
-    poisson_probabilities_num = stats.poisson.pmf(np.sort(num_of_counts_plate), np.average(num_of_counts_plate))
-    print(poisson_probabilities_num)
-    plt.plot(sorted_count_rate, poisson_probabilities_num, "gx", label="Poisson distribution")
+    poisson_probabilities = poisson_distribution(sorted_count_rate, average_count_rate)
+    plt.plot(sorted_count_rate, poisson_probabilities, "gx", label="Poisson distribution")
 
     gaussian_x = np.arange(range_1[0], range_1[1], 0.1)
-    gaussian_probabilities = stats.norm.pdf(gaussian_x, average_num_of_counts, np.sqrt(average_num_of_counts))
+    gaussian_probabilities = stats.norm.pdf(gaussian_x, average_count_rate, np.sqrt(average_count_rate))
     plt.plot(gaussian_x, gaussian_probabilities, label="Gaussian distribution")
     plt.show()
